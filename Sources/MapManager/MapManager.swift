e36432d91f7de5dfe4346270451b2b7dca266bec
金子光 <kaneko@foot-loose.jp>
2023-09-05 19:10:30 +0900

    初期

---
+++ Sources/MapManager/MapManager.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@available(iOS 13.0, *)
+@available(tvOS 13.0, *)
+@available(watchOS 6.0, *)
+@available(OSX 10.15, *)
+
+public struct MapManager {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
