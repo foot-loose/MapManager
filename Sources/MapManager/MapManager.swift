9cd9f9cf74ba41fb82d9425c28384975537c3e77
金子光 <kaneko@foot-loose.jp>
2023-09-06 18:51:50 +0900

    最新

--- Sources/MapManager/MapManager.swift
+++ Sources/MapManager/MapManager.swift
@@ -16,13 +16,25 @@ struct Spot: Identifiable {
     }
 }
 
-public class SourceOfTruth: ObservableObject {
+public class MapInfo: ObservableObject {
+    @Published var data: Array<Spot> = Array()
+
+    /*
     @Published var data = [
         Spot(name: "1.circle.fill", latitude: 34.38283271919265, longitude: 132.44880007268657),
         Spot(name: "2.circle.fill", latitude: 34.383132658853114, longitude: 132.44879068495646),
         Spot(name: "3.circle.fill", latitude: 34.38382910132105, longitude: 132.45210053091606),
         Spot(name: "4.circle.fill", latitude: 34.39192446685563, longitude: 132.45564996073864)
     ]
+     */
+
+    public func addMarker(name: String,latitude: Double,longitude: Double){
+        data.append(Spot(name: name, latitude: latitude, longitude: longitude))
+    }
+    
+    public func resetMaeker(){
+        data.removeAll()
+    }
 }
 
 
@@ -32,14 +44,15 @@ public struct MapManager: View {
     
     // ユーザートラッキングモードを追従モードにするための変数を定義
     @State  var trackingMode = MapUserTrackingMode.follow
+    /*
     let spotList = [
         Spot(name: "1.circle.fill", latitude: 34.38283271919265, longitude: 132.44880007268657),
         Spot(name: "2.circle.fill", latitude: 34.383132658853114, longitude: 132.44879068495646),
         Spot(name: "3.circle.fill", latitude: 34.38382910132105, longitude: 132.45210053091606),
         Spot(name: "4.circle.fill", latitude: 34.39192446685563, longitude: 132.45564996073864)
     ]
-    
-    @StateObject var sourceOfTruth = SourceOfTruth()
+    */
+    @StateObject var mapInfo = MapInfo()
     
     // var markerList: Array<Spot> = Array()
 /*
@@ -61,7 +74,7 @@ public struct MapManager: View {
         Map(coordinateRegion: $manager.region,
             showsUserLocation: true, // マップ上にユーザーの場所を表示するオプションをBool値で指定
             userTrackingMode: $trackingMode, // マップがユーザーの位置情報更新にどのように応答するかを決定
-            annotationItems: sourceOfTruth.data,
+            annotationItems: mapInfo.data,
             annotationContent: { spot in
                 MapAnnotation(coordinate: spot.coordinate, anchorPoint: CGPoint(x: 0.5, y: 0.5),
                 content: {
