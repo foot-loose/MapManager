0f5747aba81013656695cee80e233d95acfe9a73
金子光 <kaneko@foot-loose.jp>
2023-09-06 17:08:53 +0900

    最新

--- Sources/MapManager/MapManager.swift
+++ Sources/MapManager/MapManager.swift
@@ -6,6 +6,26 @@ import MapKit
 @available(watchOS 6.0, *)
 @available(OSX 10.15, *)
 
+struct Spot: Identifiable {
+    let id = UUID()
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
+
+public class SourceOfTruth: ObservableObject {
+    @Published var data = [
+        Spot(name: "1.circle.fill", latitude: 34.38283271919265, longitude: 132.44880007268657),
+        Spot(name: "2.circle.fill", latitude: 34.383132658853114, longitude: 132.44879068495646),
+        Spot(name: "3.circle.fill", latitude: 34.38382910132105, longitude: 132.45210053091606),
+        Spot(name: "4.circle.fill", latitude: 34.39192446685563, longitude: 132.45564996073864)
+    ]
+}
+
+
 public struct MapManager: View {
     
     @ObservedObject  var manager = LocationManager()
@@ -19,6 +39,10 @@ public struct MapManager: View {
         Spot(name: "4.circle.fill", latitude: 34.39192446685563, longitude: 132.45564996073864)
     ]
     
+    @StateObject var sourceOfTruth = SourceOfTruth()
+    
+    // var markerList: Array<Spot> = Array()
+/*
     struct Spot: Identifiable {
         let id = UUID()
         let name: String
@@ -28,15 +52,16 @@ public struct MapManager: View {
             CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         }
     }
-    
+    */
     public init() {
-
+        //markerList.removeAll()
     }
+    
     public var body: some View {
         Map(coordinateRegion: $manager.region,
             showsUserLocation: true, // マップ上にユーザーの場所を表示するオプションをBool値で指定
             userTrackingMode: $trackingMode, // マップがユーザーの位置情報更新にどのように応答するかを決定
-            annotationItems: spotList,
+            annotationItems: sourceOfTruth.data,
             annotationContent: { spot in
                 MapAnnotation(coordinate: spot.coordinate, anchorPoint: CGPoint(x: 0.5, y: 0.5),
                 content: {
